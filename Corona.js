ba17daaf3a81714f6784939f7f83e4e38e226ee3
daCking15 <king.2229@buckeyemail.osu.edu>
2020-03-20 21:35:55 -0400

    Mirror v1
    - added scrollable divs that react to size of window.
    - changed scale from linear to identity
    - Working on getting the left mirror to start all the way to the right.
    - Next, work on making the mirrors move together.
    - Finally, will add multivariant data

--- Corona.js
+++ Corona.js
@@ -1,22 +1,27 @@
 window.onload=function(){
-
-    var labelArea = 160;
-    var chart,
-            width = 400,
-            bar_height = 2,
-            height = bar_height * 200;
-    var rightOffset = width + labelArea;
-    //var xFrom = d3.scale.linear()
-            //.range([0, width]);
-    var xFrom = d3.scale.linear()
-            .range([0, 300]);
-    //var xTo = d3.scale.linear()
-            //.range([0, width]);
-    var xTo = d3.scale.linear()
-            .range([0, 330]);
-    var y = d3.scale.ordinal()
-            .rangeBands([20, height]);
     
+    var scroll = document.getElementById("scroll");
+    console.log(scroll.width);
+    scroll.scrollLeft = 100;
+    
+    //width of Country Middle Section
+    var labelArea = 160;
+    var textArea = 75;
+    var countryWidth = 94;
+    var textHeight = 20;
+    
+    //chart sizing variables
+    var chart,
+        width = 2500,           
+        bar_height = 2,
+        height = bar_height * 200;
+    var rightOffset = 50;
+    var xFrom = d3.scale.identity();
+    var xTo = d3.scale.identity();
+    var y = d3.scale.ordinal()
+            .rangeBands([textHeight, height]);
+    
+    //Data
     var lCol;
     var rCol;
     if (localStorage.getItem("left")==null){
@@ -32,6 +37,7 @@ window.onload=function(){
     document.getElementById("left").addEventListener("click", changeLeft);
     document.getElementById("right").addEventListener("click", changeRight);
     
+    //Date
     var date;
     if (localStorage.getItem("changeDate")==null){
         date = "22-Jan";
@@ -39,27 +45,27 @@ window.onload=function(){
         date = localStorage.getItem("changeDate");
     }
     document.getElementById("date").innerHTML = date;
-    
-    //Prompt for date
     document.getElementById("button").addEventListener("click", changeDate);
+    
+    //Button Functions
     function changeDate(){
         date = prompt("Date: ", "");
         document.getElementById("date").innerHTML = date;
         localStorage.setItem("changeDate", date);
     }
-    
     function changeLeft(){
         lCol = prompt("Data: ", "");
         localStorage.setItem("left", lCol);
     }
-    
     function changeRight(){
         rCol = prompt("Data: ", "");
         localStorage.setItem("right", rCol);
     }
 
+    //Render
     function render(data) {
 
+        //Subset of Data based on date
         var dateSlice = new Array();
         data.forEach(
             function(item){
@@ -69,11 +75,23 @@ window.onload=function(){
             }
         );
 
-        var chart = d3.select("body")
-                .append('svg')
-                .attr('class', 'chart')
-                .attr('width', labelArea + width + width)
-                .attr('height', height);
+        var leftChart = d3.select(".LeftMirrorDiv")
+            .append('svg')
+            .attr('class', 'chart')
+            .attr('width', width)
+            .attr('height', height);
+        
+        var rightChart = d3.select(".RightMirrorDiv")
+            .append('svg')
+            .attr('class', 'chart')
+            .attr('width', width)
+            .attr('height', height);
+
+        var countries = d3.select(".CountryDiv")
+            .append('svg')
+            .attr('class', 'chart')
+            .attr('width', countryWidth)
+            .attr('height', height);
 
         xFrom.domain(d3.extent(dateSlice, function (d) {
             return d[lCol];
@@ -89,11 +107,11 @@ window.onload=function(){
         var yPosByIndex = function (d) {
             return y(d.Country);
         };
-        chart.selectAll("rect.left")
+        leftChart.selectAll("rect.left")
                 .data(dateSlice)
                 .enter().append("rect")
                 .attr("x", function (d) {
-                    return width - xFrom(d[lCol]);
+                    return width - xFrom(d[lCol]/100);
                 })  
                 .attr("y", yPosByIndex)
                 .attr("class", "left")
@@ -101,11 +119,11 @@ window.onload=function(){
                     return xFrom(d[lCol]);
                 })
                 .attr("height", y.rangeBand());
-        chart.selectAll("text.leftscore")
+        leftChart.selectAll("text.leftscore")
                 .data(dateSlice)
                 .enter().append("text")
                 .attr("x", function (d) {
-                    return width - xFrom(d[lCol])-40;
+                    return width - xFrom(d[lCol]/100)-40;
                 })
                 .attr("y", function (d) {
                     return y(d.Country) + y.rangeBand() / 2;
@@ -115,10 +133,10 @@ window.onload=function(){
                 .attr("text-anchor", "end")
                 .attr('class', 'leftscore')
                 .text(function(d){return d[lCol];});
-        chart.selectAll("text.name")
+        countries.selectAll("text.name")
                 .data(dateSlice)
                 .enter().append("text")
-                .attr("x", (labelArea / 2) + width)
+                .attr("x", 45)
                 .attr("y", function (d) {
                     return y(d.Country) + y.rangeBand() / 2;
                 })
@@ -127,21 +145,21 @@ window.onload=function(){
                 .attr('class', 'name')
                 .text(function(d){return d.Country;});
 
-        chart.selectAll("rect.right")
+        rightChart.selectAll("rect.right")
                 .data(dateSlice)
                 .enter().append("rect")
-                .attr("x", rightOffset)
+                .attr("x", 0)
                 .attr("y", yPosByIndex)
                 .attr("class", "right")
                 .attr("width", function (d) {
-                    return xTo(d[rCol]);
+                    return xTo(d[rCol]/100);
                 })
                 .attr("height", y.rangeBand());
-        chart.selectAll("text.score")
+        rightChart.selectAll("text.score")
                 .data(dateSlice)
                 .enter().append("text")
                 .attr("x", function (d) {
-                    return xTo(d[rCol]) + rightOffset+55;
+                    return xTo(d[rCol]/100)+rightOffset;
                 })
                 .attr("y", function (d) {
                     return y(d.Country) + y.rangeBand() / 2;
@@ -151,9 +169,9 @@ window.onload=function(){
                 .attr("text-anchor", "end")
                 .attr('class', 'score')
                 .text(function(d){return d[rCol];});
-        chart.append("text").attr("x",width/3).attr("y", 10).attr("class","title").text(lCol);
-        chart.append("text").attr("x",width/3+rightOffset).attr("y", 10).attr("class","title").text(rCol);
-        chart.append("text").attr("x",width+labelArea/3).attr("y", 10).attr("class","title").text("Country");
+        leftChart.append("text").attr("x",width/3).attr("y", 10).attr("class","title").text(lCol);
+        rightChart.append("text").attr("x",width/3+rightOffset).attr("y", 10).attr("class","title").text(rCol);
+        countries.append("text").attr("x",width+labelArea/3).attr("y", 10).attr("class","title").text("Country");
     }
 
     function type(d) {
